10178e61a12812fc441492bc42e7f46805a1f0d1
Lina0596 <lina.dahlhaus@icloud.com>
2025-01-16 14:50:35 +0100

    added main function

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ print("Welcome :)")
 def calculate(text_input):
     if text_input[1] == "+":
         return int(text_input[0])+int(text_input[-1])
-calculation = input("What do you want to calculate?")
-answer = calculate(calculation)
-print(f"The result is {answer}")
+    elif text_input[1] == "-":
+        return int(text_input[0])-int(text_input[-1])
+    elif text_input[1] == "*":
+        return int(text_input[0])*int(text_input[-1])
+    elif text_input[1] == "/":
+        return int(text_input[0])/int(text_input[-1])
+
 
 print("")
 
+def main():
+    calculation = input("What do you want to calculate?")
+    answer = calculate(calculation)
+    print(f"The result is {answer}")
